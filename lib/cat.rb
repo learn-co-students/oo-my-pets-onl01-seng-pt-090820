2167e7759dedd44b8ade488c5edfae178ca61fec
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-05 17:14:04 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
-class Cat
-  # code goes here
+class Cat 
+  
+  attr_accessor :owner, :mood 
+  attr_reader :name
+  
+  @@cats = []
+  
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self 
+  end 
+  
+  def self.all 
+    @@cats 
+  end 
+  
+  
+
+  
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,58 @@
+require 'pry'
 class Owner
-  # code goes here
+  
+  attr_accessor :dog, :cat
+  attr_reader :name 
+  
+  @@all = []
+  
+  
+  def initialize(name)
+    @name = name 
+    save
+  end 
+  
+  def species 
+    "human"
+  end 
+  
+  def say_species 
+    "I am a human."
+  end 
+  
+def save 
+  @@all << self
+end 
+
+def self.all 
+  @@all 
+end 
+
+def self.count 
+  @@all.count 
+end 
+
+def self.reset_all 
+  @@all.clear 
+end 
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self }
+  end 
+  
+   def dogs
+    Dog.all.select {|dog| dog.owner == self }
+  end 
+  
+  def buy_cat(name)
+   Cat.new(name,owner)
+  end
+  
+  def buy_dog 
+  end
+  
+  def walk_dogs 
+    Dog.mood = "happy"
+  end 
+  
 end
