d414724d4c48c1bf25991805eb14a97fd68ac58a
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-05 18:25:14 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,11 +48,40 @@ end
    Cat.new(name,self)
   end
   
-  def buy_dog 
+  def buy_dog(name)
+    Dog.new(name,self)
   end
   
   def walk_dogs 
-    Dog.mood = "happy"
+    Dog.all.each do |dog|
+      dog.mood = 'happy'
+    end 
   end 
   
-end
+  def feed_cats 
+    Cat.all.each do |cat|
+      cat.mood = 'happy'
+    end 
+  end 
+  
+  
+  def sell_pets 
+    Cat.all.map do |cat|
+      cat.mood = 'nervous'
+      cat.owner = nil 
+      end
+      Dog.all.map do |dog|
+      dog.mood = 'nervous'
+      dog.owner = nil 
+    end 
+  end 
+  
+  def list_pets 
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+
+
+end 
+
+
+
