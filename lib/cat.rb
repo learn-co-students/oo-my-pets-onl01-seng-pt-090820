20703d1fafad9866df1545736bf200724b996a8d
Chase090 <carloyam21@icloud.com>
2020-10-08 21:29:52 -0700

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
 class Cat
+  attr_accessor :owner, :mood
+  attr_reader :name 
   # code goes here
+@@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+
+def name_of_cat
+  @name.dup.freeze
+end
+
+def mood_of_dog
+  @mood.dup.freeze
+end
+
+def self.all
+  @@all
+end
+
+
+
 end
