855cdf1683ed04b03d06b7250f2e569b07eab560
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-14 01:33:05 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
-  # code goes here
+  attr_accessor :Cat, :mood
+  
+  def initialize (cat)
+    cat = cat.new
+    @cat = cat
+  end
+  
+  def walking (cat)
+    @walking = walking
+    if owner walking cat
+      puts 'walking'
+    else nil
+  end
+    
+end
+  def mood (cat)
+    if walking cat
+      puts "cat happy"
+    else puts "cat nervous"
+    end
+  end  
 end
